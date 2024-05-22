c8f1d2a7230720d5e5ad22d470563430e6bdde56
Laurent Brusa <wrmultitudes@gmail.com>
2024-05-22 14:51:43 +0200

    NORM(darray): 5 functions in file

---
+++ src/darray/darray3.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   darray3.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 13:40:09 by lbrusa            #+#    #+#             */
+/*   Updated: 2024/05/22 14:43:54 by lbrusa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "darray.h"
+#include "minishell.h"
+
+/*
+get the content at index - 
+returns a void pointer to the element
+*/
+void	*darray_get(t_darray *array, int i)
+{
+	if (i >= array->max)
+	{
+		debug("darray attempt to get past max");
+		return (NULL);
+	}
+	return (array->contents[i]);
+}
+
+/*
+setting the element at index to null and returning what we removed
+*/
+void	*darray_remove(t_darray *array, int i)
+{
+	void	*el;
+
+	el = array->contents[i];
+	array->contents[i] = NULL;
+	return (el);
+}
+
+/*
+create memory allocation with calloc for a darray
+works with constant size elements like ints and floats
+for strings I would allocate them and then add them to the array
+with darray_push or set
+*/
+void	*darray_new_elem(t_darray *array)
+{
+	if (array->element_size == 0)
+	{
+		debug("Can't use darray_new_elem on 0 size darrays.");
+		return (NULL);
+	}
+	return (ft_calloc(1, array->element_size));
+}
+
+/*
+*/
+int	darray_contract(t_darray *array)
+{
+	int	new_size;
+
+	if (array->end < (int)array->expand_rate)
+		new_size = (int)array->expand_rate;
+	else
+		new_size = array->end;
+	return (darray_resize(array, new_size + 1, array->max));
+}
