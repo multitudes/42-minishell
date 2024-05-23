a1d16a697353fcdeba5d046433a5d766810f525d
Laurent Brusa <wrmultitudes@gmail.com>
2024-05-23 07:21:18 +0200

    TEST(parser) added

--- tests/parser_tree_tests.cpp
+++ tests/parser_tree_tests.cpp
@@ -606,6 +606,26 @@ const char *test_parser_tree_simple_command9() {
 	result = process_ast_node(ast->right->left, NODE_TERMINAL, WORD, "comm2");
 	result = process_ast_node(ast->right->right, NODE_TERMINAL, WORD, "comm3");
 
+	//try (a) | ((b) || c)
+	lexemes = initialiser("(a) | ((b) || c)");
+	current = lexemes;
+	result = process_token(&current, &i, "(a)", EXPRESSION);
+	result = process_token(&current, &i, "|", PIPE);
+	result = process_token(&current, &i, "((b) || c)", EXPRESSION);
+
+	// this is how I check for the end of the list
+	result = process_token(&current, &i, NULL, NULL_TOKEN);
+
+	ast = create_ast(lexemes);
+
+	//here I need to walk the tree and check the nodes
+	result = process_ast_node(ast, NODE_PIPELINE, PIPE, "|");
+	result = process_ast_node(ast->left, NODE_TERMINAL, WORD, "a");
+	result = process_ast_node(ast->right, NODE_LIST, OR_IF, "||");
+	result = process_ast_node(ast->right->left, NODE_TERMINAL, WORD, "b");
+	result = process_ast_node(ast->right->right, NODE_TERMINAL, WORD, "c");
+	
+
 	return result;
 }
 
