c4bbb017fa11bb13d151e1189b25074f9a2f95f7
projektphoenix <rpriess@student.42berlin.de>
2024-05-24 16:12:14 +0200

    Added expansion for environmental variables $ENV

--- include/analyser.h
+++ include/analyser.h
@@ -22,6 +22,7 @@ extern "C" {
 # include "minishell.h"
 
 void	analyse_expand(t_ast_node *ast, t_data *data);
+void	expand_variable(t_data *data, t_token *token);
 void	which_ast_node(t_ast_node *ast);
 
 #  ifdef __cplusplus
--- src/analyser.c
+++ src/analyser.c
@@ -51,24 +51,49 @@ void	which_ast_node(t_ast_node *ast)
 		debug("not TERMINAL");
 }
 
+void	expand_variable(t_data *data, t_token *token)
+{
+		char	*key;
+		char	*temp;
+
+		debug("expand_variable");
+		key = token->lexeme;
+		temp = ft_strdup(token->lexeme = mini_get_env(data, key + 1));
+		free(key);
+		token->lexeme = temp;
+}
 /*
 the function of the analyser is to walk on the tree and analyze and expand 
 the nodes that need to.
-need to expand the env variables, the ~, the $?, the $!, the $PATH, etc..)
-(.. ) $(..) ${..} $'..' $".."
+Required is expansion of env variables (incl $PATH) and of $?
+Optional are the ~, $!, etc..)
+Also optional appear to be $(..) ${..} $'..' $".."
 I replicate here the code for the print_ast function which is in parser.c
 and walks the tree. The idea is to walk through each node... expand
 and then pass it to the executor
 */
 void analyse_expand(t_ast_node *ast, t_data *data)
 {
-	(void)data;
+	t_token	*token;
+
+	//(void)data;
+	token = ast->token_list->content;
 	if (ast == NULL)
 		return ;
 	debug("analyse expand");
+	print_ast(ast, 0);
+	debug("token type: %d", (token->type));
+	debug("ast node type: %d", ast->type);
 	// assignng a ast node type to the node
-	 which_ast_node(ast);
-	//if (ast->type == VAR_EXPANSION)
+	// which_ast_node(ast);
+	if (token->type == VAR_EXPANSION && ast->type == NODE_TERMINAL)
+		expand_variable(data, token);
+	if (token->type == )
+	if (ast->left)
+		analyse_expand(ast->left, data);
+	if (ast->right)
+		analyse_expand(ast->right, data);
+	print_ast(ast, 0);
 	//{
 		//remove "$"
 		//check if followed by "?", then replace ast->tokenlist->content with exit status of last command
