8858a6c89fa12bc2936c38bb12503edc7e9d9d04
lbrusa <lbrusa@student.42berlin.de>
2024-05-28 18:28:28 +0200

    FIX better error messages using errno

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 11:22:23 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/05/28 13:01:05 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/05/28 18:27:51 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ Updating the global makes sense when I am in the parent process.
 */
 int	error_set_status(char *msg, int status)
 {
-	write(2, msg, ft_strlen(msg));
+	perror(msg);
 	g_exit_status = status;
 	return (status);
 }
@@ -32,6 +32,6 @@ will exit the program with an error message
 */
 int	exit_err_failure(char *msg)
 {
-	write(2, msg, ft_strlen(msg));
+	perror(msg);
 	exit(EXIT_FAILURE);
 }
