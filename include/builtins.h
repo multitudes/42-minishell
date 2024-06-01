7a20a2996bc6234d9b394754d3287ddce4520ecf
lbrusa <lbrusa@student.42berlin.de>
2024-06-01 12:30:55 +0200

    FIX echo builtin update the header

--- include/builtins.h
+++ include/builtins.h
@@ -27,12 +27,14 @@ extern "C" {
 # include "history.h"
 # include <unistd.h>
 
-int	execute_builtin(t_list *tokenlist, t_data *data);
-int	execute_cd_builtin(t_data *data, t_list *tokenlist);
-int	execute_echo_builtin(t_list *tokenlist);
-int	execute_env_builtin(t_data *data);
-int	execute_export_builtin(t_data *data, t_list *tokenlist);
-int	execute_pwd_builtin(void);
+int		execute_builtin(t_list *tokenlist, t_data *data);
+int		execute_cd_builtin(t_data *data, t_list *tokenlist);
+int		execute_echo_builtin(t_list *tokenlist);
+int		execute_env_builtin(t_data *data);
+int		execute_export_builtin(t_data *data, t_list *tokenlist);
+int		execute_pwd_builtin(void);
+bool	allowed_flags(const char *flag_lexem, const char *allowed_flags);
+bool	write_data(int fd, const void *str, int *status);
 
 #  ifdef __cplusplus
 
