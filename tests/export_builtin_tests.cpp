20796a4dc50309e3973293512714214627e1372b
projektphoenix <rpriess@student.42berlin.de>
2024-06-05 20:57:07 +0200

    FIX [export builtin test] reintroduced basic test for export builtin

--- tests/export_builtin_tests.cpp
+++ tests/export_builtin_tests.cpp
@@ -19,19 +19,18 @@ const char *test_export()
 	env_arr = NULL;
 	if (!init_env_darray(&env_arr))
 		return ("error in init env array");
-	tokenlist = tokenizer('export VAR1=string VAR2="string" VAR3= VAR4="" VAR5=2=3 VAR6');
+	tokenlist = tokenizer("export VAR1=first var1=second pwd= PWD=newPWD VAR5=2=3 VAR6");
 	status = execute_export_builtin(env_arr, tokenlist);
-
 	debug("VAR1 >>>>>>>>>>>>>>> -%s-", mini_get_env(env_arr, "VAR1"));
-	my_assert(ft_strncmp(mini_get_env(env_arr, "VAR1"), "string", 7) == 0, "'export VAR1=string' did not correctly add 'string' string to VAR1");
+	my_assert(ft_strncmp(mini_get_env(env_arr, "VAR1"), "first", 6) == 0, "export VAR1=first did not correctly add value first to VAR1");
 	debug("VAR2 >>>>>>>>>>>>>>> -%s-", mini_get_env(env_arr, "VAR2"));
-	my_assert(ft_strncmp(mini_get_env(env_arr, "VAR2"), "string", 7) == 0, "export VAR2=\"string\" did not correctly add 'string' string to VAR2");
+	my_assert(ft_strncmp(mini_get_env(env_arr, "var1"), "second", 7) == 0, "export var1=second did not correctly add value second to var1");
 	debug("VAR3 >>>>>>>>>>>>>>> -%s-", mini_get_env(env_arr, "VAR3"));
-	my_assert(ft_strncmp(mini_get_env(env_arr, "VAR3"), "", 1) == 0, "'export VAR3=' did not add empty string value to VAR3");
+	my_assert(ft_strncmp(mini_get_env(env_arr, "pwd"), "", 1) == 0, "export pwd= did not add pwd and empty string value to pwd");
 	debug("VAR4 >>>>>>>>>>>>>>> -%s-", mini_get_env(env_arr, "VAR4"));
-	my_assert(ft_strncmp(mini_get_env(env_arr, "VAR4"), "", 1) == 0, "export VAR4=\"\" did not add empty string value to VAR4");
+	my_assert(ft_strncmp(mini_get_env(env_arr, "PWD"), "newPWD", 7) == 0, "export PWD=newPWD did not correctly change value of PWD to newPWD");
 	debug("VAR5 >>>>>>>>>>>>>>> -%s-", mini_get_env(env_arr, "VAR5"));
-	my_assert(ft_strncmp(mini_get_env(env_arr, "VAR5"), "2=3", 4) == 0, "'export VAR5=2=3' did not correctly add '2=3' string to VAR5");
+	my_assert(ft_strncmp(mini_get_env(env_arr, "VAR5"), "2=3", 4) == 0, "export VAR5=2=3 did not correctly add VAR5 and value 2=3");
 	debug("VAR6 >>>>>>>>>>>>>>> -%s-", mini_get_env(env_arr, "VAR6"));
 	my_assert(mini_get_env(env_arr, "VAR6") == NULL, "'export VAR6' falsely added VAR6 to env array");
 	my_assert(status == 0, "export return status fail");
@@ -51,9 +50,6 @@ const char *all_tests()
 
 // works as a main
 RUN_TESTS(all_tests);
-
-
-
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
