d69d0f8d191bf45a9861f7f2764fefbef914d98d
lbrusa <lbrusa@student.42berlin.de>
2024-06-09 09:41:45 +0200

    TESTS tilde

--- tests/tilde_scanner_tests.cpp
+++ tests/tilde_scanner_tests.cpp
@@ -57,8 +57,6 @@ const char* test_tilde_tokens() {
 	result = process_token(&current, &i, "~/Documents", PATHNAME);
 	result = process_token(&current, &i, "~/Desktop", PATHNAME);
 
-	
-
 	// this is how I check for the end of the list
 	result = process_token(&current, &i, NULL, NULL_TOKEN);
 
@@ -70,19 +68,18 @@ const char* test_tilde_tokens2() {
 	// i want to check the output of the call to the function in scanner.c file
 	// tokenizer(char *input) returning a t_list of lexemes
 	// I will create a string and check the output of the function
-	std::string str = "~- ~-/ ~-/notadirectore ~-qwerty ~-qwerty/ ~-qwerty/123 ";
+	std::string str = "~- ~-/ ~-/notadirectory ~-qwerty ~-qwerty/ ~-qwerty/123 ";
 	const char* input = str.c_str();
 	t_list *lexemes = tokenizer(input);
 	t_list *current = lexemes;
 	const char *result = NULL;
 	int i = 0;
 
-	result = process_token(&current, &i, "~", TILDE);
-	result = process_token(&current, &i, "~~", WORD);
-	result = process_token(&current, &i, "/home/username", PATHNAME);
-	result = process_token(&current, &i, "~/Documents", TILDE);
-	result = process_token(&current, &i, "~/Desktop", TILDE);
-
+	result = process_token(&current, &i, "~-", PATHNAME);
+	result = process_token(&current, &i, "~-/", PATHNAME);
+	result = process_token(&current, &i, "~-/notadirectory", PATHNAME);
+	result = process_token(&current, &i, "~-qwerty", WORD);
+	result = process_token(&current, &i, "~-qwerty/", PATHNAME);
 	
 
 	// this is how I check for the end of the list
@@ -96,7 +93,8 @@ const char *all_tests()
 	suite_start();
 	
 	// run the tests
-	run_test(test_tilde_tokens);	
+	run_test(test_tilde_tokens);
+	run_test(test_tilde_tokens2);
 	
 
 	return NULL;
