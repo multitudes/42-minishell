d634c345b9d1aa4d5d3dbc5a83f0face7160b333
lbrusa <lbrusa@student.42berlin.de>
2024-06-09 10:04:45 +0200

    TESTS added tests for ~+

--- tests/tilde_scanner_tests.cpp
+++ tests/tilde_scanner_tests.cpp
@@ -20,9 +20,9 @@ const char* process_token(t_list **current, int *i, const char* expected_lexeme,
 	
     t_token *token = (t_token *)(*current)->content;
 	
-    debug("\ntoken number %d ", *i);
-    debug("token type: %d ", token->type);
-    debug("token lexeme: -%s- \n", token->lexeme);
+    debug("\ngot token number %d ", *i);
+    debug("got token type: %d ", token->type);
+    debug("got token lexeme: -%s- \n", token->lexeme);
     my_assert(token->type == expected_tokentype, "token type is not the expected type..." );
     my_assert(strcmp(token->lexeme, expected_lexeme) == 0, "token lexeme is not expected");
     *current = (*current)->next;
@@ -80,13 +80,45 @@ const char* test_tilde_tokens2() {
 	result = process_token(&current, &i, "~-/notadirectory", PATHNAME);
 	result = process_token(&current, &i, "~-qwerty", WORD);
 	result = process_token(&current, &i, "~-qwerty/", PATHNAME);
-	
+	result = process_token(&current, &i, "~-qwerty/123", PATHNAME);
 
 	// this is how I check for the end of the list
 	result = process_token(&current, &i, NULL, NULL_TOKEN);
 
 	return result;
 }
+
+
+/*
+same as above but with ~+
+*/
+const char * test_tilde_tokens3() {
+	
+	// i want to check the output of the call to the function in scanner.c file
+	// tokenizer(char *input) returning a t_list of lexemes
+	// I will create a string and check the output of the function
+	std::string str = "~+ ~+~/ ~+/home/username ~+/Documents ~+/Desktop ~+qwerty ~+qwerty/ ~+qwerty/123";
+	const char* input = str.c_str();
+	t_list *lexemes = tokenizer(input);
+	t_list *current = lexemes;
+	const char *result = NULL;
+	int i = 0;
+
+	result = process_token(&current, &i, "~+", PATHNAME);
+	result = process_token(&current, &i, "~+~/", PATHNAME);
+	result = process_token(&current, &i, "~+/home/username", PATHNAME);
+	result = process_token(&current, &i, "~+/Documents", PATHNAME);
+	result = process_token(&current, &i, "~+/Desktop", PATHNAME);
+	result = process_token(&current, &i, "~+qwerty", WORD);
+	result = process_token(&current, &i, "~+qwerty/", PATHNAME);
+	result = process_token(&current, &i, "~+qwerty/123", PATHNAME);
+	
+	// this is how I check for the end of the list
+	result = process_token(&current, &i, NULL, NULL_TOKEN);
+
+	return result;
+}
+
 const char *all_tests()
 {
 	// necessary to start the test suite
@@ -95,7 +127,7 @@ const char *all_tests()
 	// run the tests
 	run_test(test_tilde_tokens);
 	run_test(test_tilde_tokens2);
-	
+	run_test(test_tilde_tokens3);
 
 	return NULL;
 }
