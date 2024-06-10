81cb84348c29f87c75d1aa653a937dfe60a73959
Laurent Brusa <wrmultitudes@gmail.com>
2024-06-10 16:23:05 +0200

    TEST GH actions

--- tests_integration/builtin_pwd_tests.cpp
+++ tests_integration/builtin_pwd_tests.cpp
@@ -36,7 +36,7 @@ const char* test_pwd_on_startup()
 {
     bool pass = false;
 	uint8_t	exit_status;
-	std::string output = "minishell $ echo $PWD\n" + get_current_directory();
+	std::string output = "minishell $ echo $PWD\n" + get_current_directory() + "\n";
     exit_status = run_command_and_check_output("echo $PWD", output, &pass);
 	my_assert(exit_status == 0, "Minishell exited with non-zero status");
 	my_assert(pass, "Output is not as expected");
