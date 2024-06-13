661c1d1f133226fee88468fa28d70095c54176ef
lbrusa <lbrusa@student.42berlin.de>
2024-06-13 08:59:23 +0200

    REFACTORED darray headers

--- src/darray/darray.c
+++ src/darray/darray.c
@@ -10,18 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "darray.h"
+#include <libft.h>
 
 /*
-Returns a new darray with the given element size and initial max size
-otherwise returns NULL
-The darray is a dynamic array that can store any type of data
-when it is full I create a new array adding DEFAULT_EXPAND_RATE 
-to the max size
-element_size is for ex the size of an int or depending which 
-element I store in my array
-If adding ints they need to be preallocated with darray_new
-strings can be malloced and added with darray_push
+ * Returns a new darray with the given element size and initial max size
+ * otherwise returns NULL
+ * The darray is a dynamic array that can store any type of data
+ * when it is full I create a new array adding DEFAULT_EXPAND_RATE 
+ * to the max size
+ * element_size is for ex the size of an int or depending which 
+ * element I store in my array 
+ * If adding ints they need to be preallocated with darray_new
+ * strings can be malloced and added with darray_push
 */
 t_darray	*darray_create(size_t element_size, size_t initial_max)
 {
@@ -39,8 +40,9 @@ t_darray	*darray_create(size_t element_size, size_t initial_max)
 }
 
 /*
-frees the contents of the array , set every pointer to NULL
-and sets the end to 0
+ * the content is an array of pointers. Frees the content pointed to.
+ * sets every pointer to NULL from zero to max
+ * and sets the end to 0
 */
 void	darray_clear(t_darray *array)
 {
@@ -62,10 +64,11 @@ void	darray_clear(t_darray *array)
 }
 
 /*
-In C, if you call realloc with a null pointer as the first argument, 
-it behaves exactly like malloc. It simply allocates a new block of 
-memory of the specified size. It doesn't need to 
-copy or free anything because there's no existing block of memory.
+ * reimplementation of the realloc function but we need the extra param.
+ * In C, if you call realloc with a null pointer as the first argument, 
+ * it behaves exactly like malloc. It simply allocates a new block of 
+ * memory of the specified size. It doesn't need to 
+ * copy or free anything because there's no existing block of memory.
 */
 void	*ft_realloc(void *ptr, size_t size, size_t old_size)
 {
@@ -85,6 +88,8 @@ void	*ft_realloc(void *ptr, size_t size, size_t old_size)
 }
 
 /*
+ * Called by darray_expand it will reallocate the old array and 
+ * returns -1 in case of mem failure and keep the old size
 */
 int	darray_resize(t_darray *array, size_t newsize, size_t old_size)
 {
@@ -94,12 +99,17 @@ int	darray_resize(t_darray *array, size_t newsize, size_t old_size)
 	contents = ft_realloc(array->contents, array->max * sizeof(void *), \
 	old_size * sizeof(void *));
 	if (!contents)
+    {
+        array->max = old_size;
 		return (-1);
-	array->contents = contents;
+    }
+    array->contents = contents;
 	return (0);
 }
 
 /*
+ * In our darray implementation the expansion is additive and stored 
+ * in the expand_rate
 */
 int	darray_expand(t_darray *array)
 {
