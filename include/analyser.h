c2ac4ea8bb8ec84375b201f36c57a6f1fe577190
projektphoenix <rpriess@student.42berlin.de>
2024-06-17 14:21:39 +0200

    FIX [~ expansion] improved check for next token

--- include/analyser.h
+++ include/analyser.h
@@ -28,6 +28,7 @@ typedef struct  s_expansion_flags {
 
 void	analyse_expand(t_ast_node *ast, t_data *data);
 void	expand_tokenlist(t_data *data, t_list *tokenlist);
+void	execute_expansion_by_type(t_data *data, t_list *tokenlist, t_exp_flags *flags);
 void	expand_dollar(t_darray *env_arr, t_token *token);
 char	*get_key(char *str);
 char	*replace_dollar_vars(t_darray *env_arr, char *lexeme);
@@ -39,10 +40,11 @@ void	expand_double_quotes(t_data *data, t_token *token);
 char	*get_home(t_darray *env_arr);
 int     peek_is_valid_path(char c);
 bool    valid_tilde_separator(char sep, int pos_equal_sep);
-bool	valid_tilde_expansion(t_list *tokenlist);
-char	*replace_tilde_in_str(char *str, char *home, t_exp_flags *flags);
+bool	valid_tilde_expansion(t_list *tokenlist, int index);
+char	*replace_tilde_in_str(t_list *tokenlist, char *str, char *home, t_exp_flags *flags);
 void	which_ast_node(t_ast_node *ast);
 int	    count_chars_in_str(char *str, char c);
+void	set_flags(t_list *tokenlist, t_exp_flags *flags);
 void	reset_flags(t_exp_flags *flags);
 
 #  ifdef __cplusplus
