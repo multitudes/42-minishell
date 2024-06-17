5c7fc46187edf1521aaa027ccd9be2eebce2056a
lbrusa <lbrusa@student.42berlin.de>
2024-06-17 16:15:14 +0200

    FIX echo ~

--- src/analyser/analyser2.c
+++ src/analyser/analyser2.c
@@ -6,7 +6,7 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 09:22:07 by rpriess           #+#    #+#             */
-/*   Updated: 2024/06/17 15:54:18 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/06/17 16:14:04 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ void	set_flags(t_list *tokenlist, t_exp_flags *flags)
 
 void	reset_flags(t_exp_flags *flags)
 {
-	flags->equal_status = 0;
+	flags->equal_status = 1;
 	flags->starts_var_exp = false;
 }
