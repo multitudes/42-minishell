1e41fe8563423e13fc9b7f77b4fef71d71b03f41
lbrusa <lbrusa@student.42berlin.de>
2024-06-17 15:43:45 +0200

    REFACTOR tilde expansion

--- src/analyser/analyser2.c
+++ src/analyser/analyser2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   analyser2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpriess <rpriess@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 09:22:07 by rpriess           #+#    #+#             */
-/*   Updated: 2024/06/17 09:25:12 by rpriess          ###   ########.fr       */
+/*   Updated: 2024/06/17 15:18:00 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	set_flags(t_list *tokenlist, t_exp_flags *flags)
 {
-	if (ft_strchr(get_token_lexeme(tokenlist), '=') && get_token_lexeme(tokenlist)[0] == '$')
+	if (get_token_type(tokenlist) == VAR_EXPANSION)
 		flags->dollar_exp_front = true;
 	if (ft_strchr(get_token_lexeme(tokenlist), '=') && flags->pos_equal_sep == 0)
 		flags->pos_equal_sep = 1;
