62dcb4610d3f2c45a4a0c8f43df267697f155e0e
lbrusa <lbrusa@student.42berlin.de>
2024-06-28 12:03:43 +0200

    REFACTOR globbing

--- include/globbing.h
+++ include/globbing.h
@@ -6,7 +6,7 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 09:50:37 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/06/04 14:00:19 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/06/28 12:03:25 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ extern "C" {
 
 bool	match_files_in_directory(t_darray *files, const char *pat);
 bool	is_glob_match(const char *pat, const char *file_name);
+bool 	globbing_loop(t_darray *files, const char *pat, t_globbing gl);
 
 #  ifdef __cplusplus
 
