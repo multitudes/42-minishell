dab6b0932df55b7d8d79a9a88d1658daf844d74f
lbrusa <lbrusa@student.42berlin.de>
2024-06-28 19:32:20 +0200

    NORMED globbing

--- src/globbing/globbing1.c
+++ src/globbing/globbing1.c
@@ -6,23 +6,22 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 19:16:09 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/06/28 19:22:19 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/06/28 19:32:05 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "globbing.h"
 #include "error.h"
 
-
 /*
  * I need to read the files in my directory and check if they match the pattern
  * this function will push a list of files in my darray and return true if it 
  * was successful
 */
-bool	match_files_in_directory(t_darray *files, const char *pat) 
+bool	match_files_in_directory(t_darray *files, const char *pat)
 {
-	t_globbing gl;
-	bool	result;
+	t_globbing	gl;
+	bool		result;
 
 	result = true;
 	if (!init(&gl))
@@ -40,15 +39,15 @@ bool	match_files_in_directory(t_darray *files, const char *pat)
 */
 bool	is_glob_match(const char *pat, const char *file_name)
 {
-    if (*pat == '\0')
-        return (*file_name == '\0');
-    if (*pat == '?')
-        return (*file_name != '\0' && is_glob_match(pat + 1, file_name + 1));
-    if (*pat == '*') 
+	if (*pat == '\0')
+		return (*file_name == '\0');
+	if (*pat == '?')
+		return (*file_name != '\0' && is_glob_match(pat + 1, file_name + 1));
+	if (*pat == '*')
 	{
-        if (is_glob_match(pat + 1, file_name))
-			return 1;
-        return (*file_name != '\0' && is_glob_match(pat, file_name + 1));
-    }
-    return (*pat == *file_name && is_glob_match(pat + 1, file_name + 1));
+		if (is_glob_match(pat + 1, file_name))
+			return (true);
+		return (*file_name != '\0' && is_glob_match(pat, file_name + 1));
+	}
+	return (*pat == *file_name && is_glob_match(pat + 1, file_name + 1));
 }
