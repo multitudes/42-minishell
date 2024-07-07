2bdefecc3e9e0f89878c3f8bab4a384b91878b45
lbrusa <lbrusa@student.42berlin.de>
2024-07-07 13:44:00 +0200

    TESTING

--- tests_integration/popen_tests.cpp
+++ tests_integration/popen_tests.cpp
@@ -11,42 +11,7 @@
 
 // forward declaration
 bool isRunningOnGitHubActions();
-
-/*
-util function to read from minishell using the popen call and the single command mode
-*/
-uint8_t run_command_and_check_output(const std::string& command_to_exec, std::ostringstream& result) 
-{
-    debug("running test_popen\n");
-    fflush(stdout);
-
-    std::string command = "../minishell -c \"" + command_to_exec + "\"";
-    FILE *fp = popen(command.c_str(), "r");
-    if (fp == NULL) {
-        perror("popen");
-        return false;
-    }
-
-    std::array<char, 128> buffer;
-    while (fgets(buffer.data(), buffer.size(), fp) != NULL) {
-        result << buffer.data();
-    }
-
-	int status = pclose(fp);
-	if (status == -1) {
-		perror("pclose");
-		return 1;
-	} else {
-		if (WIFEXITED(status)) {
-			printf("Exit status: %d\n", WEXITSTATUS(status));
-		} else {
-			printf("Command did not terminate normally\n");
-		}
-	}
-    debug("result: -%s- status %d", result.str().c_str(), status);
-
-    return 0;
-}
+uint8_t run_command_and_check_output(const std::string& command_to_exec, std::ostringstream& result);
 
 
 const char* test_popen() 
@@ -155,15 +120,17 @@ const char* test_echo3()
 
 const char *all_tests()
 {
-	// necessary to start the test suite
-	suite_start();
-	
-	// run the tests
-	run_test(test_popen);
-	//run_test(test_echo);
-	run_test(test_echo2);
-	run_test(test_echo3);
-
+	if (isRunningOnGitHubActions())
+	{
+		// necessary to start the test suite
+		suite_start();
+		
+		// run the tests
+		run_test(test_popen);
+		//run_test(test_echo);
+		run_test(test_echo2);
+		run_test(test_echo3);
+	}
 	return NULL;
 }
 
@@ -173,4 +140,41 @@ bool isRunningOnGitHubActions()
 {
 	const char* github_actions = std::getenv("GITHUB_ACTIONS");
 	return github_actions != NULL && strcmp(github_actions, "true") == 0;
+}
+
+
+/*
+util function to read from minishell using the popen call and the single command mode
+*/
+uint8_t run_command_and_check_output(const std::string& command_to_exec, std::ostringstream& result) 
+{
+    debug("running test_popen\n");
+    fflush(stdout);
+
+    std::string command = "../minishell -c \"" + command_to_exec + "\"";
+    FILE *fp = popen(command.c_str(), "r");
+    if (fp == NULL) {
+        perror("popen");
+        return false;
+    }
+
+    std::array<char, 128> buffer;
+    while (fgets(buffer.data(), buffer.size(), fp) != NULL) {
+        result << buffer.data();
+    }
+
+	int status = pclose(fp);
+	if (status == -1) {
+		perror("pclose");
+		return 1;
+	} else {
+		if (WIFEXITED(status)) {
+			printf("Exit status: %d\n", WEXITSTATUS(status));
+		} else {
+			printf("Command did not terminate normally\n");
+		}
+	}
+    debug("result: -%s- status %d", result.str().c_str(), status);
+
+    return 0;
 }
