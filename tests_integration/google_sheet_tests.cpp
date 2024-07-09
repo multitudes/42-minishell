02a3b009c7846c7f9f14387b4960014e0f2a0cfa
Laurent Brusa <wrmultitudes@gmail.com>
2024-07-09 16:45:30 +0200

    FIX failing LICENSE file permissions

--- tests_integration/google_sheet_tests.cpp
+++ tests_integration/google_sheet_tests.cpp
@@ -472,6 +472,19 @@ const char* test_exit2()
 {
     fflush(stdout);
 
+	int fd = open("LICENCE", O_CREAT | O_RDWR, 0644);
+    if (file_descriptor == -1) {
+        std::cerr << "Failed to open or create file." << std::endl;
+        return "error in file creation";
+    }
+	// Now explicitly set the file permissions to rw-r--r--
+    if (chmod("LICENCE", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) == -1) {
+        std::cerr << "Failed to set file permissions." << std::endl;
+        // Close the file descriptor
+        close(file_descriptor);
+        return "error in chmod";
+    }
+	
     std::ostringstream result;
 	std::string arg = "./LICENSE";
 	uint8_t exit_status = run_command_and_check_output(arg, result);
