a3e30d8b377cc0393f68543dbc298874ca53314b
lbrusa <lbrusa@student.42berlin.de>
2024-07-17 09:27:50 +0200

    ADD makefile tmt dir permissions

--- src/scanner/scanner_error.c
+++ src/scanner/scanner_error.c
@@ -6,7 +6,7 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 19:07:56 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/07/16 15:46:02 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/07/17 09:07:29 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,32 @@
 #include "splash.h"
 #include "utils.h"
 #include "splash_error.h"
+
+/*
+ * again for overview. This is the data struct
+ * living inside the scanner. It holds 
+ * - the input
+ * - error flag to stop the loop
+ * - error string to print the error
+ * - the token list to return to the parser or free
+ * 
+ * struct s_mini_data 
+ * {
+ * 	const char	*input;
+ * 	t_list		*tokenlist;
+ * 	bool		scanner_error;
+ * 	char		*scanner_err_str;
+ * };
+ * typedef struct s_mini_data t_mini_data;
+ */
+int	init_scanner_data(t_mini_data *data, const char *input)
+{
+	data->input = input;
+	data->tokenlist = NULL;
+	data->scanner_error = false;
+	data->scanner_err_str = NULL;
+	return (1);
+}
 
 /*
  * why returning true. I want to tell the scanner that the 
