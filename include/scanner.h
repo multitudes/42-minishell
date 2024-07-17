891fd09640d8c24175bbf84c00db6ee4d2a4c680
lbrusa <lbrusa@student.42berlin.de>
2024-07-17 18:45:40 +0200

    REFACTOR remove print tokenlist

--- include/scanner.h
+++ include/scanner.h
@@ -6,7 +6,7 @@
 /*   By: lbrusa <lbrusa@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 19:55:16 by lbrusa            #+#    #+#             */
-/*   Updated: 2024/07/17 13:49:29 by lbrusa           ###   ########.fr       */
+/*   Updated: 2024/07/17 18:45:19 by lbrusa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -194,7 +194,6 @@ bool	is_reserved(t_mini_data *data, char *identifier, int *start);
 bool	is_true_false(t_mini_data *data, char *str, int *start);
 bool	is_builtin(t_mini_data *data, char *identifier, int *start);
 bool	add_token(t_mini_data *data, int *i, const char *lxm, t_tokentype type);
-void	print_tokenlist(t_list *tokenlist);
 bool	is_io_number(const char *identifier);
 bool	str_is_number(const char *identifier);
 bool	str_is_alphanum(const char *identifier);
